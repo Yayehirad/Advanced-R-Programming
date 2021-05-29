6f212e3e250540220b2667e909a76fb36f94d11a
Yayehirad Melsew <078yayu@gmail.com>
2021-05-30 01:37:59 +1000

    day 1

---
+++ Week1_AdvancedR.R
@@ -0,0 +1,21 @@
+---
+title: "AdvancedR_JHU01"
+author: "Yayehirad A Melsew"
+date: "29/05/2021"
+output: html_document
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+```{r}
+# Install swirl
+install.packages("swirl")
+packageVersion("swirl")
+library(swirl)
+#Install the R Programming Environment course
+install_course("Advanced R Programming")
+swirl()
+```
+Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
